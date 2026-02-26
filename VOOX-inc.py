e91e83a473217dca6dc11c07d3474b83d8c98d60
Patrick Pinheiro <patrickkaua730@gmail.com>
2026-02-26 09:55:28 -0300

    Atualizar o VOOX-inc.py

--- VOOX-inc.py
+++ VOOX-inc.py
@@ -1,3 +1,3 @@
-https://g.co/gemini/share/cbd6e1230db5
+https://g.co/gemini/share/8f324b6713c5
 
-https://chatgpt.com/share/6999c25a-00c0-8011-8829-e62884de7c9f
+https://g.co/gemini/share/7fe305a7f83d
