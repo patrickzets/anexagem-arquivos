22908c7fe70dff450fd976c8eae08644f4c474b4
Patrick Pinheiro <patrickkaua730@gmail.com>
2026-02-27 09:25:05 -0300

    Atualizar o VOOX-inc.py

--- VOOX-inc.py
+++ VOOX-inc.py
@@ -3,3 +3,5 @@ https://g.co/gemini/share/8f324b6713c5
 https://g.co/gemini/share/7fe305a7f83d
 
 https://drive.google.com/file/d/1_Ol54C7CCSiILXOUtIeXm7TNUR42Rby5/view?usp=drivesdk
+
+https://www.mediafire.com/file/572lztgfrhez32t/captura_posicao_mouse_f8.exe/file
